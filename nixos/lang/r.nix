b079a519a35fe2072ff6210affcbaa7a99b04d21
GTrunsec <gtrunsec@hardenedlinx.org>
2019-10-26 00:31:14 -0700

    update R-cran

--- nixos/lang/r.nix
+++ nixos/lang/r.nix
@@ -1,9 +1,52 @@
 { pkgs, ... }:
 with pkgs;
 let
-  R-with-my-packages = rWrapper.override{ packages = with rPackages;[
+  
+  R-with-my-packages = rstudioWrapper.override{ packages = with rPackages;[
+    
         bookdown
         devtools
+        (let
+          llr = buildRPackage {
+            name = "llr";
+            src = pkgs.fetchFromGitHub {
+              owner = "dirkschumacher";
+              repo = "llr";
+              rev = "0a654d469af231e9017e1100f00df47bae212b2c";
+              sha256 = "0ks96m35z73nf2sb1cb8d7dv8hq8dcmxxhc61dnllrwxqq9m36lr";
+            };
+            propagatedBuildInputs = [ rlang  knitr];
+            nativeBuildInputs = [ rlang knitr ];
+          };
+          patchwork = buildRPackage {
+            name = "patchwork";
+            src = pkgs.fetchFromGitHub {
+              owner = "thomasp85";
+              repo = "patchwork";
+              rev = "fd7958bae3e7a1e30237c751952e412a0a1d1242";
+              sha256 = "00fq520xwy1ysg4k8x48x9b0yy9wyi8y8zj6dvxjg4bwx0yyp6s4";
+            };
+            propagatedBuildInputs = [ ggplot2 ];
+            nativeBuildInputs = [ ggplot2 ]; 
+          };
+          ##emo
+          emo = buildRPackage {
+            name = "emo";
+            src = pkgs.fetchFromGitHub {
+              owner = "hadley";
+              repo = "emo";
+              rev = "02a520689861c3354f7c6b575e309280b2a3a172";
+              sha256 = "0lra8xm06qli2mvr7qnffq703jmlw3xcjy9v71v7inrgjrc0nd3s";
+            };
+            propagatedBuildInputs = [ rlang lubridate];
+            nativeBuildInputs = [ purrr crayon assertthat ]; 
+          };
+        in [
+          llr
+          patchwork
+          emo
+        ])
+        ##--------------------------------------
         dplyr
         ggplot2
         xts
@@ -40,12 +83,13 @@ let
         yaml
         summarytools
         doBy
-        ]; 
+        tidyverse
+  ]; 
     };
 in
 {
   environment.systemPackages = with pkgs; [
     R-with-my-packages
-    rstudio
+    R
   ];
 }
