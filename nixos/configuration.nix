7bd852a8446de121d6f1e851cadba7aa6b14f17e
GTrunsec <gtrunsec@hardenedlinx.org>
2019-10-30 00:39:25 -0700

    fix high cpu dhcpd

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -28,7 +28,8 @@
   
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
+  networking.useDHCP = true;
+  networking.dhcpcd.persistent = true;
   # ssd trimming
   services.fstrim.enable = true;
 
