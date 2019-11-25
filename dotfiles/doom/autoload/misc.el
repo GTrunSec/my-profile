a6f17a629326f49ad32f7699ed2773e21c00e2f4
GTrunsec <gtrunsec@hardenedlinux.org>
2019-11-24 23:22:18 -0800

    update doom emacs

---
+++ dotfiles/doom/autoload/misc.el
@@ -0,0 +1,14 @@
+(defvar current-date-time-format "%a %b %d %H:%M:%S %Z %Y"
+  "Format of date to insert with `insert-current-date-time' func
+See help of `format-time-string' for possible replacements")
+
+;;;###autoload
+(defun insert-current-date-time ()
+  "insert the current date and time into current buffer.
+Uses `current-date-time-format' for the formatting the date/time."
+       (interactive)
+       ;;(insert "==========\n")
+;       (insert (let () (comment-start)))
+       (insert (format-time-string current-date-time-format (current-time)))
+       (insert "\n")
+       )
