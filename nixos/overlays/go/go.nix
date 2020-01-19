a9a4da2ce334ac5e164876ff69f63a3c79a01d0d
GTrunsec <gtrunsec@hardenedlinux.org>
2020-01-18 19:09:06 -0800

    org pro

--- nixos/overlays/go/go.nix
+++
@@ -1,8 +0,0 @@
-self: super:
-
-# http://nbp.github.io/slides/NixCon/2017.NixpkgsOverlays/
-# https://blog.flyingcircus.io/2017/11/07/nixos-the-dos-and-donts-of-nixpkgs-overlays/
-
-{
-  gomplate     = super.callPackage ./package/gomplate {};
-}
--- nixos/virtualisation/default.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  imports = [
-    ./docker.nix
-    ./libvirtd.nix
-  ];
-}
