2fc3b653bf71505d3986220a90ed4c23724ffd1f
GTrunsec <gtrunsec@hardenedlinux.org>
2020-02-27 14:55:14 -0800

    darwin: basic path and config

--- darwin/darwin-configuration.nix
+++ darwin/darwin-configuration.nix
@@ -35,10 +35,37 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+    nix = {
+      nixPath = [
+      "darwin-config=$HOME/.config/nixpkgs/darwin/darin-configuration.nix"
+      "home-manager=$HOME/.config/nixpkgs/home-manager"
+      "darwin=$HOME/.config/nixpkgs/darwin/"
+      "nixpkgs=$HOME/src/nix/nixpkgs"
+      "ssh-config-file=$HOME/.ssh/config"
+      #"ssh-auth-sock=${xdg_configHome}/gnupg/S.gpg-agent.ssh"
+    ];
+
+    binaryCaches = [
+    ];
+
+    binaryCachePublicKeys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nsm-data-analysis.cachix.org-1:GhKJmne4IPk5C78jG980Dijd+XGFZgOGULp3UKHkJ8M="
+    ];
+
+      trustedBinaryCaches = [
+        https://cache.nixos.org
+        https://nsm-data-analysis.cachix.org
+      ];
+
+      
+
+      trustedUsers = [ "gtrun"];
+  };
 
   # You should generally set this to the total number of logical cores in your system.
   # $ sysctl -n hw.ncpu
-  nix.maxJobs = 1;
-  nix.buildCores = 1;
+  nix.maxJobs = 15;
+  nix.buildCores = 8;
 }
 
