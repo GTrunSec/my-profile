82aaa3aeda8b9584f579084ec4fcc6cbfcae943d
GTrunsec <gtrunsec@hardenedlinux.org>
2020-04-20 20:04:52 -0700

    reorg overlay

--- nixos/overlays/default.nix
+++ nixos/overlays/default.nix
@@ -1,14 +1,11 @@
 self: super:
-
 {
   zat = super.callPackage ./python/zat {};
   orgparse = super.callPackage ./python/orgparse {};
   ipynb-py-convert = super.callPackage ./python/ipynb-py-convert {};
-  #zeek = super.callPackage ./zeek {};
   vast = super.callPackage ./vast {};
   dnsproxy = super.callPackage ./system/adguard-dns.nix {};
   deepsea = super.callPackage ./go/deepsea {};
   horcrux = super.callPackage ./go/horcrux {};
   orgbabelhelper = super.callPackage ./python/orgbabelhelper {};
-  wakatime = super.callPackage ./python/wakatime {};
 }
