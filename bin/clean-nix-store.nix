01b9b4b47c004dd17f23f0f5c3bfe593195ed49f
GTrunsec <gtrunsec@hardenedlinux.org>
2020-05-05 18:16:55 -0700

    update clean nix

--- bin/clean-nix-store.nix
+++ bin/clean-nix-store.nix
@@ -1,6 +1,7 @@
 {}:
 ''
 nix-env --delete-generations 7d
+home-manager expire-generations 7d
 sudo nix-env -p /nix/var/nix/profiles/system --delete-generations 7d
 nix-collect-garbage
 nix-store --optimize
