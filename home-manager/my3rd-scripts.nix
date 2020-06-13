de58d6a4ce420d29d283a35a4d15d2784783b9c8
GTrunsec <gtrunsec@hardenedlinux.org>
2020-06-13 14:19:01 -0700

    my3rd: add agenda-html

--- home-manager/my3rd-scripts.nix
+++ home-manager/my3rd-scripts.nix
@@ -1,6 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
+  home.file.".myscript/agenda-html".source =pkgs.fetchFromGitHub {
+    owner = "dantecatalfamo";
+    repo = "agenda-html";
+    rev = "6dd1cbcd5c9743ec176e2529102ad6825d113419";
+    sha256 = "1ca5017wbzvbmpm68qrvclgphzdwhdicaqdwcc3hj5a0w5gxrhjg";
+  };
   ##https://github.com/matthewbauer/nix-bundle/commits/master
   home.file.".myscript/nix-bundle".source =pkgs.fetchFromGitHub {
     owner = "matthewbauer";
