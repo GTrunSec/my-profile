d61130057136a17429e3ab9be17e4e015cac7d0d
GTrunSec <gtrun@hardenedlinux.org>
2020-07-06 15:18:36 -0700

    using signkey

--- home-manager/setting/git.nix
+++ home-manager/setting/git.nix
@@ -4,8 +4,8 @@
     enable = true;
     userName = "GTrunSec";
     userEmail = "gtrun@hardenedlinux.org";
-#    signing.key = "20C828B69E5458A0";
-#    signing.signByDefault = true;
+    signing.key = "0x761C8EBEA940960E";
+    #signing.signByDefault = true;
     ignores = [ ".projectile" ".indium.json" ".ccls-cache"
                 ".Rhistory" ".notdeft*" ".auctex-auto" "__pycache__"
                 "vast.db" "eaf"
