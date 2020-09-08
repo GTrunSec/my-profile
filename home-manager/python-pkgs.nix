29392217e535efa8cab415879f962f6491d9b6f6
GTrunSec <gtrunsec@hardenedlinux.org>
2020-09-07 22:43:00 -0700

    update python package

--- home-manager/python-pkgs.nix
+++ home-manager/python-pkgs.nix
@@ -4,45 +4,6 @@ let
 in
 {
   config = with lib; mkMerge [
-    (mkIf pkgs.stdenv.isLinux {
-      home.packages = with nixpkgs;[
-        (python37.buildEnv.override {
-          extraLibs = with python37Packages; [
-            pytest
-            numpy
-            #orgparse
-            pytest
-            scikitlearn
-            #zat
-            matplotlib
-            sqlalchemy
-            pandas
-            #voila
-            python-language-server
-            pygments
-            #orgbabelhelper
-            wakatime
-            jupyter
-            jupyterlab
-            # emacs eafr python dep
-            shapely
-            dbus-python
-            qrcode
-            pyqt5
-            pymupdf
-            xlib
-            grip
-            pyinotify
-            pyqtwebengine
-            markdown
-            feedparser
-            #
-          ];
-          ignoreCollisions = true;
-        })
-      ];
-    })
-
     (mkIf pkgs.stdenv.isDarwin {
       home.packages = with nixpkgs;[
         (python37.buildEnv.override {
