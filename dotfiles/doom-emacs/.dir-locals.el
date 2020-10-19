5dc9ab504830eae13ac548d4d4b864f5c1f25665
GTrunSec <gtrunsec@hardenedlinux.org>
2020-10-18 18:52:37 -0700

    emacs: update dir-locals

--- dotfiles/doom-emacs/.dir-locals.el
+++ dotfiles/doom-emacs/.dir-locals.el
@@ -1,1 +1,4 @@
-((org-mode . ((eval . (setq-local org-roam-directory (expand-file-name "./"))))))
+((org-mode . ((eval . (setq-local org-roam-directory (expand-file-name "./")))
+              (eval . (setq-local org-attach-id-dir  "./.attach"))
+              (eval . (setq-local org-roam-db-location "~/.emacs.d/.local/cache/org-roam-emacs-config.db"))
+              )))
