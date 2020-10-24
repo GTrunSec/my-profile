f0c690c5b95b076a12077beab9554af3cec48f1e
GTrunSec <gtrunsec@hardenedlinux.org>
2020-10-24 15:38:15 -0700

    emacs: global snails and FIXME doom-emacs.nix

--- home-manager/programs/doom-emacs.nix
+++ home-manager/programs/doom-emacs.nix
@@ -31,11 +31,11 @@ in
       if [ ! -d "$HOME/.emacs.d" ];then
          ${pkgs.git}/bin/git clone https://github.com/GTrunSec/doom-emacs.git -b my-doom ~/.emacs.d
          if [[ "$OSTYPE" == "linux-gnu" ]]; then
+         ## FIXME: pkgs.shell can not escaped the \'\' in macos?
          sed -i 's|~/org|~/Documents/org-notes|' $HOME/.emacs.d/modules/lang/org/config.el
          sed -i 's|org-roam-directory "roam"|org-roam-directory "braindump"|' $HOME/.emacs.d/modules/lang/org/contrib/roam.el
          else
-         sed -i \'\' 's|~/org|~/Documents/org-notes|g' $HOME/.emacs.d/modules/lang/org/config.el
-         sed -i \'\' 's|org-roam-directory "roam"|org-roam-directory "braindump"|g' $HOME/.emacs.d/modules/lang/org/contrib/roam.el
+         sed -i \'\' "s|~/org|~/Documents/org-notes|g" $HOME/.emacs.d/modules/lang/org/config.el
          fi
       fi
 
@@ -47,7 +47,6 @@ in
          sed -i 's|org-roam-directory "roam"|org-roam-directory "braindump"|' $HOME/.emacs.d/modules/lang/org/contrib/roam.el
          else
          sed -i \'\' "s|~/org|~/Documents/org-notes|g" $HOME/.emacs.d/modules/lang/org/config.el
-         sed -i \'\' 's|org-roam-directory "roam"|org-roam-directory "braindump"|g' $HOME/.emacs.d/modules/lang/org/contrib/roam.el
          fi
       fi
 
