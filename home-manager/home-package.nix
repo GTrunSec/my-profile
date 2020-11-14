9995eafd9589b4db085acb23b72dcada06fee936
GTrunSec <gtrunsec@hardenedlinux.org>
2020-11-14 01:04:16 -0800

    update LS-COLORS

--- home-manager/home-package.nix
+++ home-manager/home-package.nix
@@ -1,6 +1,17 @@
 { config, lib, pkgs, ... }:
 let
   clean-nix-store = pkgs.writeScriptBin "clean-nix-store" (import ../bin/clean-nix-store.nix { });
+  LS_COLORS = pkgs.fetchgit {
+    url = "https://github.com/trapd00r/LS_COLORS";
+    rev = "6fb72eecdcb533637f5a04ac635aa666b736cf50";
+    sha256 = "0czqgizxq7ckmqw9xbjik7i1dfwgc1ci8fvp1fsddb35qrqi857a";
+  };
+  ls-colors = pkgs.runCommand "ls-colors" { } ''
+    mkdir -p $out/bin $out/share
+    ln -s ${pkgs.coreutils}/bin/ls $out/bin/ls
+    ln -s ${pkgs.coreutils}/bin/dircolors $out/bin/dircolors
+    cp ${LS_COLORS}/LS_COLORS $out/share/LS_COLORS
+  '';
 in
 {
   config = with lib; mkMerge [
@@ -17,6 +28,7 @@ in
         #for emacs notdeft
         notdeft
         #rnix-lsp
+        ls-colors
       ];
     })
 
