5a29c0e2f480d8f01742ed781cdf8586ca22e20f
GTrunSec <gtrunsec@hardenedlinux.org>
2020-12-05 15:25:00 -0800

    DONE

--- shell.nix
+++ shell.nix
@@ -8,8 +8,9 @@ pkgs.mkShell {
   buildInputs = with pkgs;[
   ];
   shellHook = ''
-    # nix-channel --add https://github.com/NixOS/nixpkgs/archive/${nixpkgs-locked.rev}.tar.gz nixpkgs
-    # nix-channel --update
+    nix-channel --add https://github.com/NixOS/nixpkgs/archive/${nixpkgs-locked.rev}.tar.gz nixpkgs
+    nix-channel --update
     home-manager build -f home.nix -I nixpkgs=${pkgsChannel}
+    exit
     '';
 }
