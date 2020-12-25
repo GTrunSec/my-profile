ee1e513d3ed364cdd73c06709b1acec12d647869
GTrunSec <gtrunsec@hardenedlinux.org>
2020-12-25 00:31:07 -0800

    emacs: update braindump files

--- dotfiles/doom-emacs/.dir-locals.el
+++ dotfiles/doom-emacs/.dir-locals.el
@@ -1,4 +1,3 @@
-((org-mode . ((eval . (setq-local org-roam-directory (expand-file-name "./")))
+((org-mode . (
               (eval . (setq-local org-attach-id-dir  "./.attach"))
-              (eval . (setq-local org-roam-db-location "~/.emacs.d/.local/cache/org-roam-emacs-config.db"))
               )))
