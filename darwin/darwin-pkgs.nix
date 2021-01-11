790df2224be41ab36891c09995f86af332fd538b
GTrunSec <gtrunsec@hardenedlinux.org>
2021-01-11 02:20:28 -0800

    emacs: remove selectrum

--- darwin/darwin-pkgs.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-let
-  darwin = (import <nix-darwin> { config.allowUnfree = true; config.ignoreCollisions = true;});
-in
-{
-  environment.systemPackages = with darwin; [
-    emacs
-  ];
-}
