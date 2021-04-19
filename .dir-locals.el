e27d8686aa4b8b98ab6a063ce7396f1d331a3e39
GTrunSec <gtrunsec@hardenedlinux.org>
2021-04-19 03:13:50 -0700

    clean up

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((org-mode . (
-              (eval . (setq-local org-attach-id-dir  "./.attach"))
-              (eval . (setq-local org-roam-db-location "~/.emacs.d/.local/cache/org-roam-dotfiles.db"))
-              )))
